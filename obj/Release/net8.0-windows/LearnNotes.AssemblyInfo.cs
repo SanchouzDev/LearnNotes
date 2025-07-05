47da53b91aadcd2f14ae6b514bd3cb7bf990d7bf
SanchouzDev <94763444+SanchouzDev@users.noreply.github.com>
2025-07-05 22:43:31 +0300

    gitignore csdix files

--- obj/Release/net8.0-windows/LearnNotes.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/LearnNotes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnNotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8551dcb13b9843d7f7e0b454b17d97f5b4ea00f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722eb05119581d80594463d2632c105c6ae81be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnNotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnNotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
